6a26394b3aa73f87952396210491481dc2423b53
Gabriela Leticia <gabriela.leticia.souza@gmail.com>
2023-10-16 19:53:16 +0200

    Kernel function implementation-no tests yet

--- learnSPH/kernel.cpp
+++ learnSPH/kernel.cpp
@@ -1,4 +1,6 @@
 #include "kernel.h"
+#include <math.h>
+#include <iostream>
 
 double learnSPH::kernel::cubic_spline(const double q)
 {
@@ -9,13 +11,17 @@ double learnSPH::kernel::cubic_spline(const double q)
 		return alpha * (2.0/3.0 - q*q + 0.5*q*q*q);
 	}
 	else if (q < 2.0) {
-		// ...
-		return 0.0;
+		return alpha * (1.0/6.0) * (2 - q) * (2 - q) * (2 - q);
 	}
 	else {
-		// ...
 		return 0.0;
 	}
+}
+
+double learnSPH::kernel::kernel_function(const double xi, const double xj, const double h)
+{
+	const double q = fabs(xi - xj) / h;
+	return (1 / (h*h*h)) * cubic_spline(q);
 }
 
 double learnSPH::kernel::cubic_grad_spline(const double q)
--- learnSPH/kernel.h
+++ learnSPH/kernel.h
@@ -9,6 +9,7 @@ namespace learnSPH
 		constexpr double PI = 3.14159265358979323846;
 		
 		double cubic_spline(const double q);
+		double kernel_function(const double xi, const double xj, const double h);
 		double cubic_grad_spline(const double q);
 	};
 };
