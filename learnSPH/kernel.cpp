247329e93dbe0618cafdc59fb24c7dbb7421fee2
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-10-20 11:55:52 +0200

    initial commit

--- learnSPH/kernel.cpp
+++ learnSPH/kernel.cpp
@@ -5,17 +5,13 @@ double learnSPH::kernel::cubic_spline(const double q)
 	assert(q >= 0.0);
 	constexpr double alpha = 3.0 / (2.0 * PI);
 
-	if (q < 1.0) {
-		return alpha * (2.0/3.0 - q*q + 0.5*q*q*q);
-	}
-	else if (q < 2.0) {
-		// ...
-		return 0.0;
-	}
-	else {
-		// ...
-		return 0.0;
-	}
+	double result = 0;
+
+	// branchless programming for the win
+	result = (alpha * (2.0/3.0 - q * q + .5*q*q*q)) * (q < 1.0);
+	result = (alpha * (1.0/6.0*(2 - q)*(2 - q)*(2 - q))) * ((1 <= q) && (q < 2.0));
+	// result stays zero if q is greater equal than 2
+	return result;
 }
 
 double learnSPH::kernel::cubic_grad_spline(const double q)
