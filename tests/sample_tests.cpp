13edaad6f7b265ca73777c5081e5d9c3d5a2e1e0
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-11-02 23:01:51 +0100

    applied kernel class, somehow breaks two tests

--- tests/sample_tests.cpp
+++ tests/sample_tests.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <cstdio>
+#include <ostream>
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
@@ -7,9 +9,13 @@
 
 #include "catch.hpp"
 
+// uncomment for clangd to link the library
+// #include "../extern/Eigen/Eigen/Dense"
 #include "../learnSPH/kernel.h"
 #include "../learnSPH/io.h"
 #include "../learnSPH/sampling.h"
+#include "../learnSPH/types.h"
+#include "../learnSPH/time_integration.h"
 
 #include <chrono>
 
@@ -21,9 +27,9 @@ struct KernelFunction
         return true;
     }
 
-    bool symmetry(Eigen::Vector3d xi, Eigen::Vector3d xj, double h)
+    bool symmetry(learnSPH::kernel::CubicSplineKernel &kernel, Eigen::Vector3d xi, Eigen::Vector3d xj)
     {       
-        return learnSPH::kernel::kernel_function(xi - xj,h) == learnSPH::kernel::kernel_function(xj - xi,h);
+        return kernel.kernel_function(xi - xj) == kernel.kernel_function(xj - xi);
     }
 
     bool delta()
@@ -31,94 +37,88 @@ struct KernelFunction
         return true;
     }
 
-    bool nonnegative(Eigen::Vector3d xi, Eigen::Vector3d xj, double h)
+    bool nonnegative(learnSPH::kernel::CubicSplineKernel &kernel, Eigen::Vector3d xi, Eigen::Vector3d xj)
     {
-        return learnSPH::kernel::kernel_function(xi - xj,h) >= 0.0;
+        return kernel.kernel_function(xi - xj) >= 0.0;
     }
 
-    bool compactness(Eigen::Vector3d xi, Eigen::Vector3d xj, double h, double beta, double tolerance)
+    bool compactness(learnSPH::kernel::CubicSplineKernel &kernel, Eigen::Vector3d xi, Eigen::Vector3d xj, double beta, double tolerance)
     {
-        if ((xi - xj).norm() > beta * h){
-            return learnSPH::kernel::kernel_function(xi - xj,h) == 0.0;
+        if ((xi - xj).norm() > beta * kernel.h){
+            return kernel.kernel_function(xi - xj) - 0 <= tolerance;
         }
         return true;
     }
 
-    bool branch01_00(double tolerance)
+    bool branch01_00(learnSPH::kernel::CubicSplineKernel &kernel,double tolerance)
     {
         // Branch 0-1
         // q = 0
         Eigen::Vector3d xi = Eigen::Vector3d(0.3, 0.3, 0.3);
         Eigen::Vector3d xj = Eigen::Vector3d(0.3, 0.3, 0.3);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) -0.0397887) < tolerance;
+        double q = (xi - xj).norm() / kernel.h;
+        return std::abs(kernel.kernel_function(xi - xj) -0.0397887) < tolerance;
     }
 
-    bool branch01_05(double tolerance)
+    bool branch01_05(learnSPH::kernel::CubicSplineKernel &kernel,double tolerance)
     {
         // Branch 0-1
         // q = 0.5
         Eigen::Vector3d xi = Eigen::Vector3d(0.0, 0.0, 0.0);
         Eigen::Vector3d xj = Eigen::Vector3d(1.0, 0.0, 0.0);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
+        double q = (xi - xj).norm() / kernel.h;
 
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) - 0.0285982) < tolerance;
+        return std::abs(kernel.kernel_function(xi - xj) - 0.0285982) < tolerance;
     }
 
-    bool branch12_10(double tolerance)
+    bool branch12_10(learnSPH::kernel::CubicSplineKernel &kernel,double tolerance)
     {
         // Branch 1-2
         // q = 1.0
         Eigen::Vector3d xi = Eigen::Vector3d(-1.0, 0.0, 0.0);
         Eigen::Vector3d xj = Eigen::Vector3d(1.0, 0.0, 0.0);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
+        double q = (xi - xj).norm() / kernel.h;
 
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) - 0.00994718) < tolerance;
+        return std::abs(kernel.kernel_function(xi - xj) - 0.00994718) < tolerance;
     }
     
-    bool branch12_15(double tolerance)
+    bool branch12_15(learnSPH::kernel::CubicSplineKernel &kernel,double tolerance)
     {
         // Branch 1-2
         // q = 1.5
         Eigen::Vector3d xi = Eigen::Vector3d(-1.0, -1.0, 0.0);
         Eigen::Vector3d xj = Eigen::Vector3d(1.0, 1.0, 1.0);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
+        double q = (xi - xj).norm() / kernel.h;
 
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) - 0.0012434) < tolerance;
+        return std::abs(kernel.kernel_function(xi - xj) - 0.0012434) < tolerance;
     }
 
-    bool branch2plus_20(double tolerance)
+    bool branch2plus_20(learnSPH::kernel::CubicSplineKernel &kernel, double tolerance)
     {
         // Branch 2+
         // q = 2.0
         Eigen::Vector3d xi = Eigen::Vector3d(-1.0, 0.0, 0.0);
         Eigen::Vector3d xj = Eigen::Vector3d(3.0, 0.0, 0.0);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
+        double q = (xi - xj).norm() / kernel.h;
 
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) - 0.0) < tolerance;
+        return std::abs(kernel.kernel_function(xi - xj) - 0.0) < tolerance;
     }
 
-    bool branch2plus_25(double tolerance)
+    bool branch2plus_25(learnSPH::kernel::CubicSplineKernel &kernel, double tolerance)
     {
         // Branch 2+
         // q = 2.44949
         Eigen::Vector3d xi = Eigen::Vector3d(-1.0, -1.0, -1.0);
         Eigen::Vector3d xj = Eigen::Vector3d(3.0, 1.0, 1.0);
-        double h = 2.0;
-        double q = (xi - xj).norm() / h;
+        double q = (xi - xj).norm() / kernel.h;
 
-        return std::abs(learnSPH::kernel::kernel_function(xi - xj,h) - 0.0) < tolerance;
+        return std::abs(kernel.kernel_function(xi - xj) - 0.0) < tolerance;
     }
 };
 
 struct GradientCubicSpline
 {
-    bool finite_differences_compare(Eigen::Vector3d xi, Eigen::Vector3d xj, double h, double tolerance)
+    bool finite_differences_compare(learnSPH::kernel::CubicSplineKernel &kernel, Eigen::Vector3d xi, Eigen::Vector3d xj, double tolerance)
     {
         // Unit vectors
         Eigen::Vector3d ex = Eigen::Vector3d(1, 0, 0);
@@ -126,18 +126,18 @@ struct GradientCubicSpline
         Eigen::Vector3d ez = Eigen::Vector3d(0, 0, 1);
 
         //Finite differences individual components 
-        double fin_diff_x = learnSPH::kernel::kernel_function(xi - xj + tolerance * ex,h) - learnSPH::kernel::kernel_function(xi - xj - tolerance * ex,h);
-        double fin_diff_y = learnSPH::kernel::kernel_function(xi - xj + tolerance * ey,h) - learnSPH::kernel::kernel_function(xi - xj - tolerance * ey,h);
-        double fin_diff_z = learnSPH::kernel::kernel_function(xi - xj + tolerance * ez,h) - learnSPH::kernel::kernel_function(xi - xj - tolerance * ez,h);
+        double fin_diff_x = kernel.kernel_function(xi - xj + tolerance * ex) - kernel.kernel_function(xi - xj - tolerance * ex);
+        double fin_diff_y = kernel.kernel_function(xi - xj + tolerance * ey) - kernel.kernel_function(xi - xj - tolerance * ey);
+        double fin_diff_z = kernel.kernel_function(xi - xj + tolerance * ez) - kernel.kernel_function(xi - xj - tolerance * ez);
 
         //Finite differences
         Eigen::Vector3d fin_diff = Eigen::Vector3d(fin_diff_x, fin_diff_y, fin_diff_z) / (2.0 * tolerance) ;
 
-        return (fin_diff - learnSPH::kernel::kernel_gradient(xi - xj,h)).norm() < tolerance;
+        return (fin_diff - kernel.kernel_gradient(xi - xj)).norm() < tolerance;
     }
 
-    bool gradient_zero_distance(Eigen::Vector3d x, double h, double tolerance){
-        return learnSPH::kernel::kernel_gradient(x - x,h) == Eigen::Vector3d(0, 0, 0);
+    bool gradient_zero_distance(learnSPH::kernel::CubicSplineKernel &kernel, Eigen::Vector3d x, double tolerance){
+        return kernel.kernel_gradient(x - x) == Eigen::Vector3d(0, 0, 0);
     }
 };
 
@@ -160,25 +160,31 @@ TEST_CASE( "Tests for our kernel function", "[kernel]" )
     const double EPSILON = 0.000001;
 
     SECTION("Testing the branches of the kernel function") {
-        REQUIRE(kernel.branch01_00(EPSILON));
-        REQUIRE(kernel.branch01_05(EPSILON));
-        REQUIRE(kernel.branch12_10(EPSILON));
-        REQUIRE(kernel.branch12_15(EPSILON));
-        REQUIRE(kernel.branch2plus_20(EPSILON));
-        REQUIRE(kernel.branch2plus_25(EPSILON));
+
+        learnSPH::kernel::CubicSplineKernel cubicSpline(2.0);
+
+        REQUIRE(kernel.branch01_00(cubicSpline, EPSILON));
+        REQUIRE(kernel.branch01_05(cubicSpline, EPSILON));
+        REQUIRE(kernel.branch12_10(cubicSpline, EPSILON));
+        REQUIRE(kernel.branch12_15(cubicSpline, EPSILON));
+        REQUIRE(kernel.branch2plus_20(cubicSpline, EPSILON));
+        REQUIRE(kernel.branch2plus_25(cubicSpline, EPSILON));
     }
     
     SECTION("Testing properties of the kernel function"){
         double beta = 2.0;
+        double h = std::abs(dis(gen)) * 5;
+        learnSPH::kernel::CubicSplineKernel cubicSpline2(h);
+
         for(int i = 0 ; i < 1000; i++)
         {
-            double h = std::abs(dis(gen)) * 5;
             Eigen::Vector3d xi = Eigen::Vector3d(dis(gen), dis(gen), dis(gen));
             Eigen::Vector3d xj = Eigen::Vector3d(dis(gen), dis(gen), dis(gen));
-
-            REQUIRE(kernel.compactness(xi,xj,h,beta,EPSILON));
-            REQUIRE(kernel.symmetry(xi,xj,h));
-            REQUIRE(kernel.nonnegative(xi,xj,h));
+            
+            REQUIRE(kernel.symmetry(cubicSpline2, xi,xj));
+            REQUIRE(kernel.nonnegative(cubicSpline2, xi,xj));
+            //doesn't work with class cubic spline
+            REQUIRE(kernel.compactness(cubicSpline2, xi,xj,beta,EPSILON));
         }
         
         REQUIRE(kernel.unity());
@@ -193,8 +199,11 @@ TEST_CASE( "Tests for our kernel function", "[kernel]" )
             Eigen::Vector3d xi = Eigen::Vector3d(dis(gen), dis(gen), dis(gen));
             Eigen::Vector3d xj = Eigen::Vector3d(dis(gen), dis(gen), dis(gen));
 
-            REQUIRE(gradient.finite_differences_compare(xi,xj,h,EPSILON));
-            REQUIRE(gradient.gradient_zero_distance(xi,h,EPSILON));
+            learnSPH::kernel::CubicSplineKernel cubicSpline(h);
+
+            REQUIRE(gradient.gradient_zero_distance(cubicSpline, xi, EPSILON));
+            // Doesn't work with class cubicSpline
+            REQUIRE(gradient.finite_differences_compare(cubicSpline, xi,xj, EPSILON));
         }
     }
 }
