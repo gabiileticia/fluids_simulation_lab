ca0f263b0497ee73e22c31696a8113ee48f3661e
Gabriela Leticia <gabriela.leticia.souza@gmail.com>
2023-11-04 23:07:19 +0100

    First work on densities function

---
+++ learnSPH/densities.cpp
@@ -0,0 +1,62 @@
+#include "densities.h"
+#include <iostream>
+
+#include "../extern/CompactNSearch/include/CompactNSearch/CompactNSearch"
+#include "../learnSPH/kernel.h"
+
+
+void learnSPH::densities::compute_boundary_masses(
+    std::vector<double>& output,
+    std::vector<Eigen::Vector3d>& boundary_particles,
+    unsigned int point_set_id,
+    CompactNSearch::PointSet const& pointset,
+    const double density,
+    double h) 
+{
+    for (int i = 0; i < pointset.n_points(); ++i)
+    {
+        double kernel_sum = 0.0;
+    
+        for (size_t j = 0; j < pointset.n_neighbors(point_set_id, i); ++j)
+        {
+            const unsigned int pid = pointset.neighbor(point_set_id, i, j);
+            kernel_sum += learnSPH::kernel::kernel_function(boundary_particles[i] - boundary_particles[pid],h);
+        }
+        output[i] = density / kernel_sum;
+    }
+}
+
+void learnSPH::densities::compute_fluid_density(
+    std::vector<double>& output,
+    std::vector<Eigen::Vector3d>& particles,
+    std::vector<Eigen::Vector3d>& boundary_particles,
+    std::vector<double>& boundary_particles_masses,
+    unsigned int point_set_id_fluid,
+    CompactNSearch::PointSet const& ps_fluid,
+    unsigned int point_set_id_boundary,
+    CompactNSearch::PointSet const& ps_boundary,
+    const double fluid_mass,
+    double h
+    ) 
+{
+    for (int i = 0; i < ps_fluid.n_points(); ++i)
+    {
+        double density_sum = 0.0;
+
+        // Get fluid neighbors of fluid point set.
+        for (size_t j = 0; j < ps_fluid.n_neighbors(point_set_id_fluid, i); ++j)
+        {
+            const unsigned int pid = ps_fluid.neighbor(point_set_id_fluid, i, j);
+            density_sum += fluid_mass * learnSPH::kernel::kernel_function(particles[i] - particles[pid],h);
+        }
+
+        // Get boundary neighbors of fluid point set.
+        for (size_t j = 0; j < ps_fluid.n_neighbors(point_set_id_boundary, i); ++j)
+        {
+            const unsigned int pid = ps_fluid.neighbor(point_set_id_boundary, i, j);
+            density_sum += boundary_particles_masses[pid] * 
+                            learnSPH::kernel::kernel_function(particles[i] - boundary_particles[pid],h);
+        }
+        output[i] = density_sum;
+    }
+}
---
+++ learnSPH/densities.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <array>
+#include <vector>
+
+#include <Eigen/Dense>
+#include "../extern/CompactNSearch/include/CompactNSearch/CompactNSearch"
+
+
+namespace learnSPH {
+    namespace densities {
+        void compute_boundary_masses(
+            std::vector<double>& output,
+            std::vector<Eigen::Vector3d>& boundary_particles,
+            unsigned int point_set_id,
+            CompactNSearch::PointSet const& pointset,
+            const double density,
+            double h);
+        void compute_fluid_density(
+            std::vector<double>& output,
+            std::vector<Eigen::Vector3d>& particles,
+            std::vector<Eigen::Vector3d>& boundary_particles,
+            std::vector<double>& boundary_particles_masses,
+            unsigned int point_set_id_fluid,
+            CompactNSearch::PointSet const& ps_fluid,
+            unsigned int point_set_id_boundary,
+            CompactNSearch::PointSet const& ps_boundary,
+            const double fluid_mass,
+            double h
+            );
+    }
+}
