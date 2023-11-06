08aa3163f78bfead73ea06e67473dda2898e1d2b
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-11-06 18:09:42 +0100

    refactored, no more particle struct

--- learnSPH/time_integration.cpp
+++ learnSPH/time_integration.cpp
@@ -3,25 +3,31 @@
 #include <Eigen/Dense>
 #include <cassert>
 #include <cmath>
+#include <cstdio>
+#include <iostream>
 #include <vector>
 
 #include "types.h"
 
 learnSPH::timeIntegration::semiImplicitEuler::semiImplicitEuler(double dt,
-                                                                double radius,
-                                                                double v_max) {
+                                                                double radius) {
   assert(dt <=
          0.5 * radius / std::pow(std::pow(v_max, 2) + std::pow(v_max, 2), 0.5));
-  dt = dt;
-  vec_dt = Eigen::Vector3d(dt, dt, dt);
-  radius = radius;
-  v_max = v_max;
+  this->dt = dt;
+  this->radius = radius;
+  this->v_max = 0;
 }
 
 void learnSPH::timeIntegration::semiImplicitEuler::integrationStep(
-    std::vector<learnSPH::types::Particle> &particles) {
-  for (int i = 0; i < particles.size(); i++) {
-    particles[i].velocity = particles[i].velocity + vec_dt * gravity;
-    particles[i].pos = particles[i].pos + dt * particles[i].velocity;
+    std::vector<Eigen::Vector3d> &positions,
+    std::vector<Eigen::Vector3d> &velocity,
+    std::vector<Eigen::Vector3d> &forces) {
+  for (int i = 0; i < positions.size(); i++) {
+
+    velocity[i] = velocity[i] + dt * (gravity + forces[i]);
+    positions[i] = positions[i] + dt * velocity[i];
+
+    if (velocity[i].norm() > v_max)
+      v_max = velocity[i].norm();
   }
 }
