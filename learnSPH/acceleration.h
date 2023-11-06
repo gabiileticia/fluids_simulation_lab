e10b317ebb2151741dc57c5c59fdb6197d7664c6
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-11-06 19:33:23 +0100

    for computing acceleration

---
+++ learnSPH/acceleration.h
@@ -0,0 +1,16 @@
+
+
+#ifndef ACCELERATION
+#define ACCELERATION
+
+namespace learnSPH {
+namespace acceleration {
+class Acceleration{
+    double B;   // stiffness constant
+    
+};
+}
+}
+
+
+#endif
