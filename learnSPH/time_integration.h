8708db4e02b0485026c98a15bdb31d4d374716b2
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-11-06 18:09:57 +0100

    refactor nore more particle struct

--- learnSPH/time_integration.h
+++ learnSPH/time_integration.h
@@ -10,12 +10,11 @@ namespace timeIntegration {
 class semiImplicitEuler {
 public:
   double dt;
-  Eigen::Vector3d vec_dt;
   double radius;
   double v_max;
-  const double gravity = -9.81;
-  semiImplicitEuler(double dt, double radius, double v_max);
-  void integrationStep(std::vector<learnSPH::types::Particle> &particles);
+  const Eigen::Vector3d gravity = {0,0,-9.81};
+  semiImplicitEuler(double dt, double radius);
+  void integrationStep(std::vector<Eigen::Vector3d> &positions, std::vector<Eigen::Vector3d> &velocity, std::vector<Eigen::Vector3d> &forces);
 };
 } // namespace timeIntegration
 } // namespace learnSPH
--- learnSPH/types.h
+++ learnSPH/types.h
@@ -4,11 +4,7 @@
 
 namespace learnSPH {
 namespace types {
-struct Particle {
-  Eigen::Vector3d pos;
-  Eigen::Vector3d velocity;
-  Eigen::Vector3d force;
-};
+  
 }
 }
 
