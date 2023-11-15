ff98252c04e6d6ee7203112c33edb19a5f7ccbb7
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-11-15 14:15:26 +0100

    fix and reformat

--- app/dam_break.cpp
+++ app/dam_break.cpp
@@ -21,8 +21,8 @@ int main()
     // declaring variables beforehand to make assignments less cluttered
     unsigned int point_set_id_boundary, point_set_id_fluid, size_particles, size_boundary;
     int count_del;
-    double particle_radius, a, b, c, dt_cfl, dt, dt_default, t_next_frame, t_between_frames,
-        t_simulation, B, v_f, v_b, fluid_particle_mass;
+    double particle_radius, dt_cfl, dt, dt_default, t_next_frame, t_between_frames, t_simulation, B,
+        v_f, v_b, fluid_particle_mass;
     Eigen::Vector3d fluid_begin, fluid_end, boundary_begin, boundary_end, gravity;
     std::vector<Eigen::Vector3d> particles_positions, particles_accelerations, particles_velocities,
         boundary_particle_positions;
@@ -43,28 +43,21 @@ int main()
 
     // Fluid settings
     const double fluid_rest_density = 1000.0;
-    fluid_begin                     = {0.05, 0.05, 0.05};
-    fluid_end                       = {.1, .25, .5};
+    fluid_begin                     = {0.0, 0.0, 0.0};
+    fluid_end                       = {.15, .25, .5};
     const double fluid_volume       = (fluid_end.x() - fluid_begin.x()) *
                                 (fluid_end.y() - fluid_begin.y()) *
                                 (fluid_end.z() - fluid_begin.z());
     const double fluid_mass = fluid_volume * fluid_rest_density;
 
     const std::string boundary_file = "./res/boundary.obj";
-    a                               = .15;
-    b                               = .8;
-    c                               = 1.0;
-    boundary_begin                  = {.0, .0, .0};
-    boundary_end                    = {a, b, c};
-    const double particle_volume =
-        4.0 / 3.0 * learnSPH::kernel::PI * particle_radius * particle_radius * particle_radius;
-    const double boundary_volume = 2 * particle_volume * (a * b + a * c + b * c);
-    const double boundary_mass   = boundary_volume * fluid_rest_density;
+    boundary_begin                  = {-.02, -.02, -.02};
+    boundary_end                    = {0.17, 0.8, 1.0};
 
     // simulation parameter
-    dt_default       = 0.00025;
+    dt_default       = 0.0005;
     t_next_frame     = 0;
-    t_between_frames = 0.0005;
+    t_between_frames = 0.001;
     t_simulation     = 0;
     B                = 1000 * 1.02;
     v_f              = 0.0025;
@@ -109,10 +102,6 @@ int main()
     CompactNSearch::PointSet const &ps_boundary = nsearch.point_set(point_set_id_boundary);
     CompactNSearch::PointSet const &ps_fluid    = nsearch.point_set(point_set_id_fluid);
 
-    // assigning boundary masses
-    const double boundary_particles_mass   = boundary_mass / size_boundary;
-    const double boundary_particle_density = fluid_rest_density / size_boundary;
-
     // assigning correct sizes to boundary related vectors
     boundary_particles_densities.resize(boundary_particle_positions.size());
     boundary_particles_masses.resize(boundary_particle_positions.size());
@@ -136,8 +125,6 @@ int main()
         std::cout << "boundary_file: " << boundary_file << std::endl;
         std::cout << "boundary_begin: " << boundary_begin.transpose() << std::endl;
         std::cout << "boundary_end: " << boundary_end.transpose() << std::endl;
-        std::cout << "boundary_volume: " << boundary_volume << std::endl;
-        std::cout << "boundary_mass: " << boundary_mass << std::endl;
         std::cout << "dt_default: " << dt_default << std::endl;
         std::cout << "B: " << B << std::endl;
         std::cout << "v_f: " << v_f << std::endl;
@@ -148,15 +135,16 @@ int main()
         std::cout << "Number of fluid particles" << std::endl;
         std::cout << size_particles << std::endl;
         std::cout << "fluid_particle_mass: " << fluid_particle_mass << std::endl;
-        std::cout << "boundary_particles_mass: " << boundary_particles_mass << std::endl;
     }
 
     while (t_simulation < 5) {
-        std::cout << "t_simulation " << t_simulation << std::endl;
-        std::cout << "v_max " << semImpEuler.v_max << std::endl;
+        std::cout << "t_simulation " << t_simulation << "\n";
+        std::cout << "v_max " << semImpEuler.v_max << "\n";
         // compute dt
         dt_cfl = 0.5 * particle_radius * (1 / std::min(50.0, semImpEuler.v_max));
         dt     = std::min(dt_cfl, dt_default);
+
+        std::cout << "dt " << dt << "\n";
 
         // Find neighbors
         nsearch.find_neighbors();
@@ -175,13 +163,15 @@ int main()
                                    point_set_id_fluid, point_set_id_boundary, ps_fluid, ps_boundary,
                                    particles_positions, boundary_particle_positions,
                                    particles_velocities, boundary_particles_masses,
-                                   boundary_particle_density, fluid_particle_mass);
+                                   fluid_rest_density, fluid_particle_mass);
 
         // Integrate time step
         semImpEuler.integrationStep(particles_positions, particles_velocities,
                                     particles_accelerations, deleteFlags, dt, count_del);
+
         utils::deleteOutOfBounds(particles_positions, particles_velocities, particles_accelerations,
                                  particles_densities, particles_pressure, deleteFlags, count_del);
+
         nsearch.resize_point_set(point_set_id_fluid, particles_positions.front().data(),
                                  particles_positions.size());
 
