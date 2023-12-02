9c219d0b673f0c8685cb9336331c5f1503af98d2
Morten Henkel <morten.henkel@rwth-aachen.de>
2023-12-02 16:28:08 +0100

    changed to work with updated mcubes which works on level sets
    Currently not working since torus must be manualy set in mcubes

--- tests/surface_recon_tests.cpp
+++ tests/surface_recon_tests.cpp
@@ -1,38 +1,27 @@
 #include "catch.hpp"
 #include <Eigen/Dense>
+#include <cstdint>
+#include <unordered_map>
+#include <vector>
 
 #include "../learnSPH/io.h"
 #include "../learnSPH/marching_cubes.h"
-
-struct Radii
-{
-    double r;
-    double R;
-};
-
-double ImplicitTorus(Eigen::Vector3d pos, void *args)
-{
-    // pointer conversion
-    Radii *radii = ((Radii *)args);
-
-    double interval = (std::sqrt(pos.x() * pos.x() + pos.y() * pos.y()) - radii->R);
-    return radii->r * radii->r - interval * interval - pos.z() * pos.z();
-}
+#include "../learnSPH/theta_functions.h"
 
 TEST_CASE("Tests for marching cubes class", "[mcubes]")
 {
 
-    double cellWidth, epsilon;
+    double cellWidth, epsilon, c;
     // dimensions
     uint nx, ny, nz;
     Eigen::Vector3d min, max;
-    bool implicitFlag = true;
-    Radii radii;
+    bool implicitFlag = false;
 
-    radii.R   = 0.7;
-    radii.r   = 0.2;
+    double R  = 0.7;
+    double r  = 0.2;
     cellWidth = 0.04;
     epsilon   = 1e-6;
+    c         = -0.55;
 
     min = {-1, -1, -0.3};
     max = {1, 1, 0.3};
@@ -41,22 +30,33 @@ TEST_CASE("Tests for marching cubes class", "[mcubes]")
     ny = (max.y() - min.y()) / cellWidth + 1;
     nz = (max.z() - min.z()) / cellWidth + 1;
 
-    learnSPH::surface::MarchingCubes mcubes(cellWidth, nx, ny, nz, min, ImplicitTorus, &radii,
-                                            epsilon, implicitFlag);
+    std::vector<double> level_set(nx * ny * nz);
+    std::unordered_map<uint64_t, double> levelMap(nx * ny * nz);
 
-    SECTION("Testing isosurface function"){
-        mcubes.get_Isosurface();
+    learnSPH::theta_functions::Torus torus(min, r, R, cellWidth, nx, ny, nz);
+    learnSPH::surface::MarchingCubes mcubes(cellWidth, nx, ny, nz, min, torus, epsilon,
+                                            implicitFlag, c);
+
+    torus.computeLevelSet(level_set);
+    torus.computeLevelMap(levelMap);
+
+    SECTION("Testing isosurface function")
+    {
+        mcubes.get_Isosurface(level_set);
+        mcubes.get_Isosurface_sparse(levelMap);
     }
-    SECTION("Testing Normals computation"){
-        mcubes.get_Isosurface();
-        mcubes.compute_normals_alternative();
+    SECTION("Testing Normals computation")
+    {
+        mcubes.get_Isosurface(level_set);
+        mcubes.compute_normals();
     }
 
-    SECTION("Testing output of file"){
-        mcubes.get_Isosurface();
-        mcubes.compute_normals_alternative();
-        learnSPH::write_tri_mesh_to_vtk("torus.vtk", mcubes.intersections, mcubes.triangles, mcubes.intersectionNormals);
+    SECTION("Testing output of file")
+    {
+        mcubes.get_Isosurface_sparse(levelMap);
+        mcubes.compute_normals();
+        learnSPH::write_tri_mesh_to_vtk("torus.vtk", mcubes.intersections, mcubes.triangles,
+                                        mcubes.intersectionNormals);
         std::cout << "All finished";
     }
-
 }
